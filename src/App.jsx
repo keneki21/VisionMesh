5605ecf8a73e355a56bdf368bdc3517efca19d43
Muhammad Hashir <117543927+keneki21@users.noreply.github.com>
2025-10-28 21:45:32 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 
