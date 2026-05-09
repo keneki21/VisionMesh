70356f94ac1490c1b3266040ad340797cbd70506
Muhammad Hashir Afzal <117543927+keneki21@users.noreply.github.com>
2026-05-09 15:50:52 +0500

    Fix navbar import case for Linux Docker build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Evaluation from './pages/Evaluation';
